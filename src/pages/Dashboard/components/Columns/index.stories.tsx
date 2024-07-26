2168d468e74f327de4d16030ec95894de268c1d9
CamilleRamos132 <camille.gpramos@gmail.com>
2024-07-25 21:00:30 -0300

    fix(storybook): fixed columns story

--- src/pages/Dashboard/components/Columns/index.stories.tsx
+++ src/pages/Dashboard/components/Columns/index.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Collumns from './index';
+import { fn } from '@storybook/test';
 
 
 const meta = {
@@ -11,7 +12,13 @@ const meta = {
 
   },
   args: {
-    registrations: []
+    deleteRegistration: fn,
+    changeRegistrationStatus: fn,
+    separatedData: {
+      APPROVED: [],
+      REPROVED: [],
+      REVIEW: []
+    }
   },
 } satisfies Meta<typeof Collumns>;
 
@@ -21,21 +28,41 @@ type Story = StoryObj<typeof meta>;
 
 export const EmptyColumn: Story = {
   args: {
-    registrations: []
+    separatedData: {
+      APPROVED: [],
+      REPROVED: [],
+      REVIEW: []
+    }
   }
 };
 
 export const ColumnWithData: Story = {
   args: {
-    registrations: [
-      {
+    separatedData: {
+      APPROVED: [{
+        "admissionDate": "22/10/2023",
+        "email": "luiz@caju.com.br",
+        "employeeName": "Luiz Filho",
+        "status": "APPROVED",
+        "cpf": "56642105087",
+        "id": "3"
+      }],
+      REPROVED: [{
+        "id": "2",
+        "admissionDate": "22/10/2023",
+        "email": "jose@caju.com.br",
+        "employeeName": "José Leão",
+        "status": "REPROVED",
+        "cpf": "78502270001"
+      }],
+      REVIEW: [{
         "id": "1",
         "admissionDate": "22/10/2023",
         "email": "filipe@caju.com.br",
         "employeeName": "Filipe Marins",
         "status": "REVIEW",
         "cpf": "78502270001"
-      }
-    ]
+      }]
+    }
   }
 };
