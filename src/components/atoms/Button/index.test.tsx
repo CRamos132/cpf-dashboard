d265122f8aa6ccda428a2ab596991f9be283095e
CamilleRamos132 <camille.gpramos@gmail.com>
2024-07-27 19:08:20 -0300

    test(Button): added unit tests

--- src/components/atoms/Button/index.test.tsx
+++ src/components/atoms/Button/index.test.tsx
@@ -1,10 +1,23 @@
 import Button from ".";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Button", () => {
   it("Should show button", () => {
-    const { debug } = render(<Button>Ativar</Button>);
+    render(<Button>Ativar</Button>);
     expect(screen.getByRole("button", { name: /ativar/i }));
-    debug();
+  });
+  it("Should call function on click", () => {
+    const clickFuntion = jest.fn()
+    render(<Button onClick={clickFuntion}>Ativar</Button>);
+    const button = screen.getByRole("button")
+    fireEvent.click(button)
+    expect(clickFuntion).toHaveBeenCalledTimes(1)
+  });
+  it("Should not call function if disabled", () => {
+    const clickFuntion = jest.fn()
+    render(<Button onClick={clickFuntion} disabled>Ativar</Button>);
+    const button = screen.getByRole("button")
+    fireEvent.click(button)
+    expect(clickFuntion).toHaveBeenCalledTimes(0)
   });
 });
