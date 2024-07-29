204aff6c0db45f19ce493603679631967dd4d9c5
CamilleRamos132 <camille.gpramos@gmail.com>
2024-07-28 22:25:16 -0300

    refactor(types): padronized props typings

--- src/components/molecules/TextField/index.tsx
+++ src/components/molecules/TextField/index.tsx
@@ -1,41 +1,21 @@
 import { InputHTMLAttributes } from "react";
 import InputMask from 'react-input-mask';
-import styled from "styled-components";
+import * as S from "./styles";
 
-export const Input = styled.input`
-  padding: 0 8px;
-  vertical-align: middle;
-  border-radius: 2px;
-  width: 100%;
-  min-height: 36px;
-  background-color: #ffffff;
-  border: 1px solid rgba(36, 28, 21, 0.3);
-  transition: all 0.2s ease-in-out 0s;
-  font-size: 16px;
-  line-height: 18px;
-  font-weight: normal;
-  border-radius:8px;
-  :focus {
-    outline: none;
-    border: 1px solid #007c89;
-    box-shadow: inset 0 0 0 1px #007c89;
-  }
-`;
-
-type Props = {
+interface ITextField extends InputHTMLAttributes<any> {
   label?: string;
   error?: string;
   mask?: string
-} & InputHTMLAttributes<any>;
+}
 
-const TextField = (props: Props) => {
+const TextField = (props: ITextField) => {
   return (
     <div data-testid='textField'>
       <label htmlFor={props.id}>{props.label}</label>
       <InputMask mask={props.mask ?? ''} {...props}>
-        <Input />
+        <S.Input />
       </InputMask>
-      <span style={{ fontSize: 12, color: 'red' }}>{props.error}</span>
+      <S.ErrorMessage>{props.error}</S.ErrorMessage>
     </div>
   );
 };
